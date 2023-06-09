b603444496ead2ae29018f8e8aec62d21d1339ee
이승호 <82797695+koelkorea@users.noreply.github.com>
2023-06-09 17:34:26 +0900

    6월 9일 내용 정정

--- day11-1(얕은, 깊은 복사 개념정리).cpp
+++ day11-1(얕은, 깊은 복사 개념정리).cpp
@@ -3,11 +3,6 @@
 //     -> (중요) class 객체는 스스로 값을 가지는게 아니라, 메모리의 힙영역에서 객체의 저장된 위치값을 저장하는 class 포인터변수
 //         -> 따라서 별도의 조치 없다면, 객체 간 단순한 복사는 class포인터 주소값을 같게 복사하여, 같은 위치의 값들을 역참조하겠다는 의미가 되어버린다!
 //             -> 소멸자로 둘 중 하나만 해제해도 나머지 하나는 낙동강 오리알이 되어버림.. (= dangled pointer 이슈)
-// 
-//   # 얕은 복사법
-//      -> 클래스명 인스턴스명 = 타 인스턴스명	
-
-//         ex) MyString s3 = s1;     =>      s3이 가리키는 객체 위치 -> 힙영역의 객체위치 <- s1이 가르키는 객체 위치                 
 
 //   # dangled pointer (얕은 복사의 문제)
 //     : 포인터의 경우 얕은복사 방식을 사용하면 같은 메모리 주소값을를 서로 다른 포인터가 가리킴
@@ -19,7 +14,24 @@
 //     -> 이렇게 하면, 복사된 객체는 모든 데이터의 값은 대상 객체와 같더라도, 힙영역에 객체 데이터가 위치한 주소가 다르기에 원본인 대상 객체가 사라져도 영향을 받지 않는 독립성을 유지!
 
 
-//얕은 복사의 문제와 이를 해결한 깊은 복사 코드 (얕은 복사의 dangled pointer 이슈 해결)
+// class 객체 간 복사법
+//  : class명 인스턴스객체명1(인스턴스객체명2)   or  class명 인스턴스객체명1 = 인스턴스객체명2
+//     -> =연산자 오버로딩을 통해 가능하게 함
+
+//     1. 자동적으로 생성되는 복사 생성자에 아무런 조치를 취하지 않았을 경우
+//         -> 얕은 복사 작동
+       
+//            ex) MyString s3 = s1;     =>      s3이 가리키는 객체 위치 == s1이 가르키는 객체 위치    
+//                MyString s3(s1)
+       
+//     2. 복사 생성자의 내용에 동적 메모리 할당을 통해 직접 객체의 내용이 완전히 별개의 힙공간에 위치하도록 조치한 경우
+//         -> 깊은 복사 작동
+       
+//            ex) MyString s3 = s1;     =>      s3이 가리키는 객체 위치 != s1이 가르키는 객체 위치      
+//                MyString s3(s1)
+
+
+// 얕은 복사의 문제와 이를 해결한 깊은 복사 코드 (얕은 복사의 dangled pointer 이슈 해결)
 #include <iostream>
 
 using namespace std;
@@ -35,7 +47,7 @@ public:
 
 };
 
-// 복사 생성자
+// 생성자
 MyString_swallowCopy::MyString_swallowCopy(char* p) {
     pStr = new char[strlen(p) + 1];
     strcpy_s(pStr, strlen(p) + 1, p);
@@ -97,7 +109,7 @@ int main() {
     //----------------------------------------[얕은 복사]----------------------------------------------------------------------
     MyString_swallowCopy s1 = (char*)"Hello World";       // (중요!) s1을 해제할때, s3애서 이미 같은 주소값에 위치한 메모리 영역을 해제하였기에 dangling pointer 로 인한 오류발생
     MyString_swallowCopy s2 = (char*)"C++ language";
-    MyString_swallowCopy s3 = s1;   // default 복사 생성자 
+    MyString_swallowCopy s3 = s1;                         // 아무 내용이 없는 default 복사 생성자를 실행하므로, 얕은 복사를 실행함 
 
     s1.Show();
     s2.Show();
@@ -110,9 +122,7 @@ int main() {
 
     // = MyString s1((char*)"C++ language");
     MyString_deepCopy s5 = (char*)"C++ language";
-
-    // 깊은 복사 생성자가 호출되어 실행
-    MyString_deepCopy s6 = s4;
+    MyString_deepCopy s6 = s4;                            // 깊은 복사 생성자가 호출되어 실행     
 
     s4.Show();
     s5.Show();
